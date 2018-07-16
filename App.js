4ea6e3b544f5df0759b3f5a8e972963e71c20104
HDT <hdt@MacBook-Air-khxng-HDT.local>
2018-07-16 16:04:43 +0700

    add & delete

---
+++ App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class App extends React.Component {
+  state = {
+    text: '',
+    todo: ['test', 'testttt', 'yaaa']
+  }
+
+  addTodo = () => {
+    let newTodo = this.state.text
+    let arr = this.state.todo
+    arr.push(newTodo)
+    this.setState({todo: arr, text: ''})
+  }
+  deleteTodo = (items) => {
+    let arr = this.state.todo
+    let pos = arr.indexOf(items)
+    arr.splice(pos, 1)
+    this.setState({todo: arr})
+  }
+  renderTodo = () => {
+    return this.state.todo.map((items, index) => (
+      <Text 
+        key={index}
+        onPress={() => {this.deleteTodo(items)}}
+      >{items}</Text>
+    ))
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app test!</Text>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder="Type here to translate!"
+          value={this.state.text}
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Button 
+          title="Add TODO"
+          color="#841584"
+          onPress={this.addTodo}
+        />
+        {this.renderTodo()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputStyle: {
+    height: 40,
+    borderColor: "green",
+    borderWidth: 1,
+    marginTop: 20,
+  }
+});
